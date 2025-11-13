17b60a1be5cc9dd44842357386b8b12f02160330
patriciaayrah <ayrah0421.pao@gmail.com>
2025-11-13 15:43:31 +0800

    Pokemon pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/Pagination.css';
+
+
+function Pagination({ page, setPage, totalPages }) {
+  return (
+    <div className="pagination">
+      <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Previous
+      </button>
+      <span>Page {page} of {totalPages}</span>
+      <button onClick={() => setPage(page + 1)} disabled={page === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
